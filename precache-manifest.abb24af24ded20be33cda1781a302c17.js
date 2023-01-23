c76d77e846f92911d4bcb2a25248cd3ca5abe8b8
KhiavDim <49508132+khiavdim@users.noreply.github.com>
2023-01-23 15:33:13 -0800

    Updates

---
+++ precache-manifest.abb24af24ded20be33cda1781a302c17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba2f6a4f97dbe01eff649fe6b3c2175",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f984a97370c5ae60a12c",
+    "url": "/static/css/main.c3feb969.chunk.css"
+  },
+  {
+    "revision": "2e3b78fcb6d4a59d36d1",
+    "url": "/static/js/2.ba0672d9.chunk.js"
+  },
+  {
+    "revision": "f984a97370c5ae60a12c",
+    "url": "/static/js/main.850d05c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
